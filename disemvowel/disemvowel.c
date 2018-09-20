c0d27a1b406201ebec01cb3cef58a5b5d57c78a8
bella148 <bella148@morris.umn.edu>
2018-09-20 15:20:06 -0500

    Begin re-write to address multiple errors

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,38 +1,74 @@
 #include <stdlib.h>
-
+#include <stdio.h>
 #include <string.h>
 #include "disemvowel.h"
 
-bool isVowel(char letter) {
-  if(letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U' ||
-     letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' ){
-    return true;
-  }
-  else {
-    return false;
+// Determines if a character is a vowel or not
+bool isVowel(char letter) 
+{
+  // Look for capital and lowercase vowels
+  switch(letter)
+  {
+    // All vowels
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U':
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+      return true;
+      break;
+    
+    // Otherwise it isn't a vowel
+    default:
+      return false;
+      break;
   }
 }
 
-int countVowel(char *str) {
+//Counts the number of values the string
+int countVowel(char *str)
+{
   int count = 0;
-  for(unsigned int i = 0; i < strlen(str); i++){
-    if(isVowel(str[i])){
+  for(unsigned int i = 0; i < strlen(str); i++)
+  {
+    if(isVowel(str[i]))
+    {
         count++;
     }
   }
   return count;
 }
 
-
 char *disemvowel(char *str) {
-  char *result = (char*) calloc(strlen(str) - countVowel(str), sizeof(char));
-  int resultIndex = 0;
-  for(unsigned int i = 0; i < strlen(str); i++){
-	if(!isVowel(str[i])){
-		result[resultIndex] += str[i];
-		resultIndex++;
-	}
+  
+  int old_string_length = strlen(str);
+  int old_string_vowels = countVowel(str);
+  int new_string_length = old_string_length - old_string_vowels;
+  if(new_string_size <= 0)
+  {
+    int resultIndex = 0;
+    char *result = (char*) calloc(new_string_length, sizeof(char));
+    for(unsigned int i = 0; i < old_string_length; i++)
+    {
+      if(!isVowel(str[i]))
+      {
+        result[resultIndex] = str[i];
+        resultIndex++;
+      }
+    }
+    //There needs to be a check for blanks not incrementing resultIndex on blanks
+
   }
-  result[resultIndex] = '\0';
+  else
+  {
+    char *result = (char*) calloc(1, sizeof(char));
+    result[0] = '\0';
+  }
+
   return result;
 }
